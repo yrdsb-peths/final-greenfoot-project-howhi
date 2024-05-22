5f0f791bd9dd13a7801ebd933d76357c1e996e5c
howhi <howhiisahop@gmail.com>
2024-05-22 11:25:37 -0400

    Created 1 mole

---
+++ Mole1.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * One of the moles to whack.
+ * 
+ * @author Ho
+ * @version May 2024
+ */
+public class Mole1 extends Actor
+{
+    /**
+     * Act - do whatever the Mole1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
