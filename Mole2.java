0c602e44d1d371574f440237c156b7ed955d2b61
howhi <howhiisahop@gmail.com>
2024-05-28 10:53:36 -0400

    Added moles to constructor in MyWorld

--- Mole2.java
+++ Mole2.java
@@ -13,27 +13,46 @@ public class Mole2 extends Animal
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     
-    GreenfootImage[] mole2Animation = new GreenfootImage[9];    
+    GreenfootImage[] mole2Animation = new GreenfootImage[9];
     
+    SimpleTimer animationMole2Timer = new SimpleTimer();
+    
+    MyWorld gameWorld = (MyWorld) getWorld();
     public Mole2()
     {
-        setImage(mole2Animation[8]);
-        
         for(int i = 0; i < mole2Animation.length; i++)
         {
             mole2Animation[i] = new GreenfootImage("images/Mole_Animate/mole_animate" + i + ".png");
             mole2Animation[i].scale(75, 75);
         }
         
-        
+        animationMole2Timer.mark();
+        setImage(mole2Animation[8]);
     }
     
     
     public void act()
     {
+        animateMole2();
+        
         if(Greenfoot.mouseClicked(this))
         {
-            
+            setImage(mole2Animation[8]);
         }
+        
+        gameWorld.diceRollTimer.mark();
+    }
+    
+    int imageIndex = 0;
+    public void animateMole2()
+    {
+        if(animationMole2Timer.millisElapsed() < 100)
+        {
+            return;
+        }
+        
+        animationMole2Timer.mark();
+        setImage(mole2Animation[imageIndex]);
+        imageIndex = (imageIndex + 1) % mole2Animation.length;        
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -18,13 +18,32 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x300 cells with a cell size of 1x1 pixels.
         super(600, 300, 1);
+        
+        Mole2 mole2 = new Mole2();
+        addObject(mole2, getWidth() / 3, getHeight() / 3);
+        
+        Mole3 mole3 = new Mole3();
+        addObject(mole3, getWidth() / 2, getHeight() / 3);
+        
+        Mole4 mole4 = new Mole4();
+        addObject(mole4, getWidth() * (3 / 4), getHeight() / 3);
+        
+        Mole5 mole5 = new Mole5();
+        addObject(mole5, getWidth() / 3, getHeight() * (2 / 3));
+        
+        Mole6 mole6 = new Mole6();
+        addObject(mole6, getWidth() / 2, getHeight() * (2 / 3));
+        
+        Mole7 mole7 = new Mole7();
+        addObject(mole7, getWidth() * (3 / 4), getHeight() * (2 / 3));
+        
     }
     
     public void act()
     {
         diceRollTimer.mark();
         
-        if(diceRollTimer.millisElapsed() < 2000)
+        if(diceRollTimer.millisElapsed() < 5000)
         {
             return;
         }
@@ -59,37 +78,31 @@ public class MyWorld extends World
     
     public void generateMole2()
     {
-        Mole2 mole2 = new Mole2();
-        addObject(mole2, getWidth() / 3, getHeight() / 2);
+        
     }
     
     public void generateMole3()
     {
-        Mole3 mole3 = new Mole3();
-        addObject(mole3, getWidth() / 3, getHeight() / 2);
+
     }
     
         public void generateMole4()
     {
-        Mole4 mole4 = new Mole4();
-        addObject(mole4, getWidth() / 3, getHeight() / 2);
+
     }
     
         public void generateMole5()
     {
-        Mole5 mole5 = new Mole5();
-        addObject(mole5, getWidth() / 3, getHeight() / 2);
+
     }
     
         public void generateMole6()
     {
-        Mole6 mole6 = new Mole6();
-        addObject(mole6, getWidth() / 3, getHeight() / 2);
+        
     }
     
         public void generateMole7()
     {
-        Mole7 mole7 = new Mole7();
-        addObject(mole7, getWidth() / 3, getHeight() / 2);
+        
     }
 }
