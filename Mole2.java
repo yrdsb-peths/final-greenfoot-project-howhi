e734e4c8004bd25b661b4a35c7a00651d8acd3d3
howhi <howhiisahop@gmail.com>
2024-05-28 10:16:06 -0400

    set image of mole2 to hiding in hole

--- Mole2.java
+++ Mole2.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Mole2 here.
+ * One of the moles to hit/click on.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Ho 
+ * @version May 2024
  */
 public class Mole2 extends Animal
 {
