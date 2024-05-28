4b262cb21d40f8839ce5fd4e3f2d61450bcdccb2
howhi <howhiisahop@gmail.com>
2024-05-28 10:17:44 -0400

    Added 5 more moles

---
+++ Mole4.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * One of the moles to hit/click on.
+ * 
+ * @author Ho 
+ * @version May 2024
+ */
+public class Mole4 extends Animal
+{
+    /**
+     * Act - do whatever the Mole4 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
