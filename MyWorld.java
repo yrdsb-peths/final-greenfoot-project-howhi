5a6b94779c2a840b8747e785fac27fdf387fc6e5
howhi <howhiisahop@gmail.com>
2024-06-03 10:31:25 -0400

    Added score and time labels and values

--- MyWorld.java
+++ MyWorld.java
@@ -10,6 +10,11 @@ public class MyWorld extends World
 {
     public int diceRoll;
     SimpleTimer diceRollTimer = new SimpleTimer();
+    
+    Label scoreLabel;
+    Label currentScore;
+    Label currentTimeLabel;
+    Label currentTimeValue;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,10 +22,23 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x300 cells with a cell size of 1x1 pixels.
-        super(600, 300, 1);
+        super(600, 400, 1);
         
         Mole2 mole2 = new Mole2();
         addObject(mole2, getWidth() / 4, getHeight() / 3);
+        
+        scoreLabel = new Label("Score: ", 50);
+        addObject(scoreLabel, getWidth() / 7, 40);
+        
+        
+        currentScore = new Label(0, 50);
+        addObject(currentScore, 170, 40);
+        
+        currentTimeLabel = new Label("Time: ", 50);
+        addObject(currentTimeLabel, getWidth() * 3 / 4, 40);
+        
+        currentTimeValue = new Label(60, 50);
+        addObject(currentTimeValue, 530, 40);
         
         diceRoll = 0;
         
