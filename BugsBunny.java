9b8031f8d4e3361cdc92e3012a126096e14ac0cd
howhi <howhiisahop@gmail.com>
2024-06-08 18:42:45 -0400

    Updated bugs bunny class

--- BugsBunny.java
+++ BugsBunny.java
@@ -9,6 +9,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class BugsBunny extends Animal
 {
     GreenfootImage[] bugsBunnyAnimation = new GreenfootImage[3];
+    
+    SimpleTimer bugsBunnyTimer = new SimpleTimer();
+    boolean bugsBunnyIsAnimating = false;
     public BugsBunny()
     {
         for(int i = 0; i < bugsBunnyAnimation.length; i++)
@@ -16,13 +19,37 @@ public class BugsBunny extends Animal
             bugsBunnyAnimation[i] = new GreenfootImage("images/Bugs_Bunny_Animate/bugs_bunny_hammered" + i + ".png");
             bugsBunnyAnimation[i].scale(100, 75);
         }
+        
+        bugsBunnyTimer.mark();        
+        setImage(bugsBunnyAnimation[0]);
     }
+    
     /**
      * Act - do whatever the BugsBunny wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        if(bugsBunnyIsAnimating == true)
+        {
+            animateBugsBunny();
+        }
+        if(Greenfoot.mouseClicked(this))
+        {
+            bugsBunnyIsAnimating = true;
+        }
+    }
+    
+    int imageIndex = 0;
+    public void animateBugsBunny()
+    {
+        MyWorld gameWorld = (MyWorld) getWorld();
+        if(bugsBunnyTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        setImage(bugsBunnyAnimation[imageIndex]);
+        imageIndex = (imageIndex + 1) % bugsBunnyAnimation.length;
+        bugsBunnyTimer.mark();
     }
 }
