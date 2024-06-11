460bdd51f117a833ea34bcceb166ec714990d548
howhi <howhiisahop@gmail.com>
2024-06-11 10:51:12 -0400

    Updated HiddenMole comments.

--- HiddenMole.java
+++ HiddenMole.java
@@ -8,8 +8,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class HiddenMole extends Animal
 {
+    // Created a new GreenfootImage.
     GreenfootImage hiddenMoleImage = new GreenfootImage("images/mole_Hidden.png");
     
+    
+    /**
+     * The constructor for HiddenMole. Here, the image above is scaled to an 
+     * appropriate size and then this actor's image is set to that scaled image.
+     */
     public HiddenMole()
     {
         hiddenMoleImage.scale(100, 75);
