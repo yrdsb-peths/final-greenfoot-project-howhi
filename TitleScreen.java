babd002139f8148b9258eb8c986930a56824b220
howhi <howhiisahop@gmail.com>
2024-06-12 16:23:36 -0400

    Finalized TitleScreen comments

--- TitleScreen.java
+++ TitleScreen.java
@@ -12,22 +12,22 @@ public class TitleScreen extends World
     SimpleTimer instructionsTimer = new SimpleTimer();
     
     // Created a Greenfoot Sound for the background music to be played on the
-    // titleScreen and GameEndsWorld.
+    // TitleScreen and GameEndsWorld.
     GreenfootSound titleScreenGameEndsMusic = new GreenfootSound("Background Music TitleScreen & GameEndsWorld.mp3");
     
     // The first title to be shown when running the game.
     Label titleLabel = new Label("Connor's\nWhac-a-Mole!", 75);
     /**
-     * The constructor for the TitleScreen World. Create 
-     * the world, create an animated mole, create the title "titleLabel".
-     * Mark the instructionsTimer.
+     * The constructor for the TitleScreen World. Create the world, create and
+     * add the animated mole to the world, create and add the title "titleLabel"
+     * to the world, and mark the instructionsTimer.
      */  
     public TitleScreen()
     {    
         // Create a new world with 600x300 cells with a cell size of 1x1 pixels.
         super(600, 300, 1); 
         
-        // Creating an animated mole on the titleScreen.
+        // Creating and adding an animated mole on the titleScreen.
         Mole1 mole1 = new Mole1();
         addObject(mole1, getWidth() / 2, 225);
         
@@ -42,10 +42,11 @@ public class TitleScreen extends World
     public void act()
     {        
         // Play the background music all the time while in this world.
+        // Set the volume to 40.
         titleScreenGameEndsMusic.setVolume(40);
         titleScreenGameEndsMusic.play();
     
-        // After two seconds, remove titleLabel and add instructions.
+        // Two seconds after pressing "Run", remove titleLabel and add instructions.
         if(instructionsTimer.millisElapsed() < 2000)
         {
             return;
@@ -61,10 +62,13 @@ public class TitleScreen extends World
         addObject(startGameLabel, getWidth() / 2, 275);
         
         
-        // If user presses <enter> then game starts. Stop background music. Switch world to MyWorld.
+        // If user presses <enter>
         if(Greenfoot.isKeyDown("enter"))
         {
+            // Stop background music. 
             titleScreenGameEndsMusic.stop();
+            
+            // Switch world to MyWorld.
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
